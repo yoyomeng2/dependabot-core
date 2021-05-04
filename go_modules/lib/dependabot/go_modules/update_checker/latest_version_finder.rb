3a830e90ef826654604ae3f21929b08d0b3397ce
Jason Rudolph <jasonrudolph@github.com>
2021-05-04 14:14:26 -0400

    🚧 Begin extracting LatestVersionFinder for go_modules
    Co-authored-by: David McIntosh <mctofu@github.com>

---
+++ go_modules/lib/dependabot/go_modules/update_checker/latest_version_finder.rb
@@ -0,0 +1,175 @@
+# frozen_string_literal: true
+
+require "excon"
+
+# require "dependabot/bundler/update_checker"
+# require "dependabot/bundler/requirement"
+require "dependabot/shared_helpers"
+require "dependabot/errors"
+require "dependabot/go_modules/resolvability_errors"
+# require "dependabot/go_modules/update_checker/latest_version_finder/dependency_source"
+
+module Dependabot
+  module GoModules
+    class UpdateChecker
+      class LatestVersionFinder
+        RESOLVABILITY_ERROR_REGEXES = [
+          # Package url/proxy doesn't include any redirect meta tags
+          /no go-import meta tags/,
+          # Package url 404s
+          /404 Not Found/,
+          /Repository not found/,
+          /unrecognized import path/
+        ].freeze
+
+        def initialize(dependency:, dependency_files:, credentials:,
+                       ignored_versions:, raise_on_ignored: false)
+          @dependency          = dependency
+          @dependency_files    = dependency_files
+          # @repo_contents_path  = repo_contents_path
+          @credentials         = credentials
+          @ignored_versions    = ignored_versions
+          @raise_on_ignored    = raise_on_ignored
+        end
+
+        def latest_version
+          @latest_version ||= fetch_latest_version
+        end
+
+        private
+
+        attr_reader :dependency, :dependency_files, :credentials, :ignored_versions
+
+        def fetch_latest_version
+          pseudo_version_regex = /\b\d{14}-[0-9a-f]{12}$/
+          return dependency.version if dependency.version =~ pseudo_version_regex
+
+          SharedHelpers.in_a_temporary_directory do
+            SharedHelpers.with_git_configured(credentials: credentials) do
+              File.write("go.mod", go_mod.content)
+
+              # Turn off the module proxy for now, as it's causing issues with
+              # private git dependencies
+              env = { "GOPRIVATE" => "*" }
+
+              version_strings = SharedHelpers.run_helper_subprocess(
+                command: NativeHelpers.helper_path,
+                env: env,
+                function: "getVersions",
+                args: {
+                  dependency: {
+                    name: dependency.name,
+                    version: "v" + dependency.version,
+                  }
+                }
+              )
+
+              # TODO: Refactor
+              current_version = version_class.new(dependency.version)
+              candidate_versions = version_strings.select { |v| version_class.correct?(v) }
+                                                  .map { |v| version_class.new(v) }
+              prereleases, stable_releases = candidate_versions.partition { |v| v.prerelease? }
+              current_version.prerelease? ? prereleases.max : stable_releases.max
+            end
+          end
+        rescue SharedHelpers::HelperSubprocessFailed => e
+          retry_count ||= 0
+          retry_count += 1
+          retry if transitory_failure?(e) && retry_count < 2
+
+          handle_subprocess_error(e)
+        end
+
+        def handle_subprocess_error(error)
+          if RESOLVABILITY_ERROR_REGEXES.any? { |rgx| error.message =~ rgx }
+            ResolvabilityErrors.handle(error.message, credentials: credentials)
+          end
+
+          raise
+        end
+
+        def transitory_failure?(error)
+          return true if error.message.include?("EOF")
+
+          error.message.include?("Internal Server Error")
+        end
+
+        def go_mod
+          @go_mod ||= dependency_files.find { |f| f.name == "go.mod" }
+        end
+
+        # def fetch_latest_version
+        #   return dependency_source.latest_git_version_details if dependency_source.git?
+
+        #   relevant_versions = dependency_source.versions
+        #   relevant_versions = filter_prerelease_versions(relevant_versions)
+        #   relevant_versions = filter_ignored_versions(relevant_versions)
+
+        #   relevant_versions.empty? ? nil : { version: relevant_versions.max }
+        # end
+
+        def filter_prerelease_versions(versions_array)
+          return versions_array if wants_prerelease?
+
+          versions_array.reject(&:prerelease?)
+        end
+
+        def filter_ignored_versions(versions_array)
+          filtered = versions_array.
+                     reject { |v| ignore_requirements.any? { |r| r.satisfied_by?(v) } }
+          raise AllVersionsIgnored if @raise_on_ignored && filtered.empty? && versions_array.any?
+
+          filtered
+        end
+
+        def filter_lower_versions(versions_array)
+          versions_array.
+            select { |version| version > Gem::Version.new(dependency.version) }
+        end
+
+        def wants_prerelease?
+          @wants_prerelease ||=
+            begin
+              current_version = dependency.version
+              if current_version && Gem::Version.correct?(current_version) &&
+                 Gem::Version.new(current_version).prerelease?
+                return true
+              end
+
+              dependency.requirements.any? do |req|
+                req[:requirement].match?(/[a-z]/i)
+              end
+            end
+        end
+
+        def dependency_source
+          @dependency_source ||= DependencySource.new(
+            dependency: dependency,
+            dependency_files: dependency_files,
+            credentials: credentials,
+            options: options
+          )
+        end
+
+        def ignore_requirements
+          ignored_versions.flat_map { |req| requirement_class.requirements_array(req) }
+        end
+
+        def requirement_class
+          Utils.requirement_class_for_package_manager(
+            dependency.package_manager
+          )
+        end
+
+        def version_class
+          Utils.version_class_for_package_manager(dependency.package_manager)
+        end
+
+        def gemfile
+          dependency_files.find { |f| f.name == "Gemfile" } ||
+            dependency_files.find { |f| f.name == "gems.rb" }
+        end
+      end
+    end
+  end
+end
